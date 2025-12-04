0e3b8c46e1ee59ffd7e37aa5bf9ded975ba58109
MichaelPKennedy <111917176+MichaelPKennedy@users.noreply.github.com>
2025-12-04 16:04:10 -0500

    prevent flash of light theme on page load

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning style={{ backgroundColor: '#0a0a0a', colorScheme: 'dark' }}>
       <head>
         <script
           dangerouslySetInnerHTML={{
